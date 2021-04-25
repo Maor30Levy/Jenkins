05eb7ba0309ee02ce134461e02584ff93a339a27
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-04-25 12:47:31 +0300

    Delete build.sh

--- jenkins/build/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "****************************"
-echo "** Building Docker Image ***"
-echo "****************************"
-
-docker build -t maor30levy/client-test -f ./client-template ./client
