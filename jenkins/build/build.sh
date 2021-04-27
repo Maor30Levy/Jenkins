f4ff7fec51a246d1683487d037607774256cac9d
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-04-27 14:02:27 +0300

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -3,5 +3,5 @@
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
-git clone https://github.com/Maor30Levy/book-store-front-end.git
+git clone "https://github.com/Maor30Levy/$COMPONENT.git"
 docker build -t $REGISTER_USERNAME/$COMPONENT -f ./$COMPONENT/Dockerfile ./$COMPONENT
