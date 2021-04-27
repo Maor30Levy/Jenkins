4d7dd6a4872aaf84166b3e69d55d671187b7ec22
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-04-27 14:14:24 +0300

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,8 +4,6 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-echo $PWD
-ls
-#rm -rf ./$COMPONENT
+rm -rf ./$COMPONENT
 #git clone "https://github.com/Maor30Levy/$COMPONENT.git"
 #docker build -t $REGISTER_USERNAME/$COMPONENT -f ./$COMPONENT/Dockerfile ./$COMPONENT
