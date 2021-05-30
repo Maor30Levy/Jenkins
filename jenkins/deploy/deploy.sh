90f68f356903fe36e87b0db619be1a48a2350b49
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-05-30 20:58:23 +0300

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,7 @@ echo "***** Deploying Project ******"
 echo "******************************"
 echo $REGISTER_USERNAME > /tmp/.project_name
 echo $COMPONENT >> /tmp/.project_name
+echo $PORT >> /tmp/.project_name
 scp -T /tmp/.project_name ec2-user@$VM_IP:/tmp/.project_name
 scp -T ./jenkins/deploy/publish ec2-user@$VM_IP:/tmp/publish
 ssh -T ec2-user@$VM_IP "/tmp/publish"
