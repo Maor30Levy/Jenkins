7c8f0d9b64219d18110b2590d534ae8f5f9dad29
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-05-30 18:30:18 +0300

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -8,4 +8,6 @@ echo "** Logging in ***"
 echo "$PASS" | docker login -u "$REGISTER_USERNAME" --password-stdin
 echo "*** Pushing image ***"
 docker push $REGISTER_USERNAME/$COMPONENT
+
+echo "*** Removing repo from Jenkins ***"
 rm -rf ./$COMPONENT
