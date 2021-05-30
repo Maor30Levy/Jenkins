22944d9c86d1c525f5f8ba8d2225e52cc5c85d07
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-05-30 18:27:27 +0300

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-echo maven-project > /tmp/.auth
-echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth
-
-scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+ssh -T ec2-user@$VM_IP"
