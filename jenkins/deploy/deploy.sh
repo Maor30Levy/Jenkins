8293792a4bae92839ef94ea7f44c70b19c2b242c
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-05-30 20:07:14 +0300

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,4 +3,4 @@
 echo "******************************"
 echo "***** Deploying Project ******"
 echo "******************************"
-ssh -T ec2-user@$VM_IP "docker run "
+ssh -T ec2-user@$VM_IP 'docker run $REGISTER_USERNAME/$COMPONENT'
